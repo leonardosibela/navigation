43a45b670151764560a3e49b63e16fa42b24253c
leonardosibela <leonardo_sibela@hotmail.com>
2021-04-08 13:17:51 -0300

    Using deep link navigation

--- app/src/main/java/com/sibela/navigation/LoginFragment.kt
+++ app/src/main/java/com/sibela/navigation/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.sibela.navigation.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -13,6 +14,8 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     private var _binding: FragmentLoginBinding? = null
 
     private val binding get() = _binding!!
+
+    private val args: LoginFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -28,6 +31,9 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val usernameDeepLink = args.username
+        binding.editTextUsername.setText(usernameDeepLink)
 
         binding.buttonConfirm.setOnClickListener {
             val username = binding.editTextUsername.text.toString()
